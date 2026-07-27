2d955c5a4c3357529d94675244905d1ce757411b
vtu30046jagadeesh <vtu30046@veltech.edu.in>
2026-07-27 11:49:02 +0530

    Add files via upload

---
+++ task 1.java.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        List<Integer> salaries = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            salaries.add(sc.nextInt());
+        }
+
+        List<Integer> updatedSalaries = salaries.stream()
+                .map(salary -> salary + (salary * 10 / 100))
+                .collect(Collectors.toList());
+
+        for (int salary : updatedSalaries) {
+            System.out.print(salary + " ");
+        }
+
+        sc.close();
+    }
+}
